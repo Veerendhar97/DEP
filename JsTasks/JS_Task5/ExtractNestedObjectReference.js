edf3d4f66f26acd2f600c8e8a8da73cc98ca8569
Veerendhar97 <veerendhar97@gmail.com>
2020-03-21 21:32:32 +0530

    Task5

---
+++ JsTasks/JS_Task5/ExtractNestedObjectReference.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/extract-nested-object-reference
+
+Object.prototype.hash = function(string) {
+  var array = string.split(".");
+  myObj = obj;
+  for (var i = 0; i < array.length; i++) {
+    if (myObj[array[i]] == undefined) {
+      return undefined;
+    } else {
+      myObj = myObj[array[i]];
+    }
+  }
+  return myObj;
+};
