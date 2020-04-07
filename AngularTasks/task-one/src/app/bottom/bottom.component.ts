dd58dd2bee3e1c6eccb5334acae91e9e3a88cd7f
Veerendhar97 <veerendhar97@gmail.com>
2020-04-07 17:51:37 +0530

    task1 of ng

---
+++ AngularTasks/task-one/src/app/bottom/bottom.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-bottom",
+  templateUrl: "./bottom.component.html",
+  styleUrls: ["./bottom.component.css"]
+})
+export class BottomComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
